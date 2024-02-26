816771c6ea84376515b341a2b9ab7c43e6931cda
Markus Sünkens <markussuenkens@live.de>
2024-02-26 23:25:47 +0100

    add dashboard

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,38 @@
+return {
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Set header
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
+
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "  New File", "<cmd>ene<CR>"),
+			dashboard.button("f", "󰱼  Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("e", "  Open File Explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("t", "  Find Text", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("o", "  Recently Opened Files", "<cmd>Telescope oldfiles<cr>"),
+			-- dashboard.button("r", "󰁯 Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  Quit NVIM", "<cmd>qa<CR>"),
+		}
+
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
+}
